a63a980d56e5b0dc89963c62bc192e9e52d5caef
Sonu Kumar <111670267+Code7x0@users.noreply.github.com>
2025-11-10 12:11:27 +0530

    Fix typo in Nav component name

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='w-full h-20 bg-black text-purple-400 flex items-center justify-center text-3xl font-semibold'>Artifex</div>
+    <div className='w-full h-20 bg-black text-purple-400 flex items-center justify-center text-3xl font-semibold'>Artiflex</div>
   )
 }
 
